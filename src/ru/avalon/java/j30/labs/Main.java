f34dcfa73a3da5d0747b1f72cb3623a5580e2d6c
Ильин А.Д <56357608+sashaspb09@users.noreply.github.com>
2020-02-22 08:51:32 +0300

    Update Main.java

--- src/ru/avalon/java/j30/labs/Main.java
+++ src/ru/avalon/java/j30/labs/Main.java
@@ -1,89 +1,156 @@
 package ru.avalon.java.j30.labs;
 
+import java.io.*;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Properties;
+import java.sql.Statement;
+import java.util.*;
 
-/**
- * Лабораторная работа №3
- * <p>
- * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle Certified Professional Java Programmer"
- * <p>
- * Тема: "JDBC - Java Database Connectivity" 
- *
- * @author Daniel Alpatov <danial.alpatov@gmail.com>
- */
 public class Main {
 
-    /**
-     * Точка входа в приложение
-     * 
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SQLException {
-        /*
-         * TODO #01 Подключите к проекту все библиотеки, необходимые для соединения с СУБД.
-         */
-        try (Connection connection = getConnection()) {
-            ProductCode code = new ProductCode("MO", 'N', "Movies");
-            code.save(connection);
+    public static HashMap<String, String> sqlMap;
+
+    public static void main(String[] args) throws SQLException, IOException {
+
+        Connection connection;
+        if (loadDriver()) {                                                    // работаем с базой
+
+            
+            System.out.println("Trying to get connection");
+            connection = getConnection();
+            System.out.println("Прогресс conn = " + connection);
+            //------------------------------------------------------ 
+            sqlMap = getQueries("sql/queries.sql");
+            printAllQuieries(sqlMap); // для отладки загрузки строк запросов
+
+            System.out.println("*****Работа с базой*****");
+            ProductCode product = new ProductCode(1, "SW", 1.5);
+            System.out.println("============done Product================");
+
+            product.save(connection);
+
             printAllCodes(connection);
 
-            code.setCode("MV");
-            code.save(connection);
+            product.save(connection);
+
             printAllCodes(connection);
+
+        
         }
-        /*
-         * TODO #14 Средствами отладчика проверьте корректность работы программы
-         */
     }
+
     /**
      * Выводит в кодсоль все коды товаров
-     * 
+     *
      * @param connection действительное соединение с базой данных
-     * @throws SQLException 
-     */    
+     * @throws SQLException
+     */
     private static void printAllCodes(Connection connection) throws SQLException {
         Collection<ProductCode> codes = ProductCode.all(connection);
         for (ProductCode code : codes) {
             System.out.println(code);
         }
     }
+
     /**
      * Возвращает URL, описывающий месторасположение базы данных
-     * 
+     *
      * @return URL в виде объекта класса {@link String}
      */
     private static String getUrl() {
-        /*
-         * TODO #02 Реализуйте метод getUrl
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+       return  "jdbc:hsqldb:~/db1";
     }
+
     /**
      * Возвращает параметры соединения
-     * 
-     * @return Объект класса {@link Properties}, содержащий параметры user и 
+     *
+     * @return Объект класса {@link Properties}, содержащий параметры user и
      * password
      */
-    private static Properties getProperties() {
-        /*
-         * TODO #03 Реализуйте метод getProperties
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream in = ClassLoader.getSystemResourceAsStream("resources/database.properties")) {
+            properties.load(in);
+            return properties;
+        }
     }
     /**
      * Возвращает соединение с базой данных Sample
-     * 
+     *
      * @return объект типа {@link Connection}
-     * @throws SQLException 
+     * @throws SQLException
      */
-    private static Connection getConnection() throws SQLException {
-        /*
-         * TODO #04 Реализуйте метод getConnection
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private static Connection getConnection() throws IOException {
+
+        String url = getUrl();
+        Connection conn = null;
+        String user = getProperties().getProperty("user");
+        String password = getProperties().getProperty("password");
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Соединение получено: " + conn);
+
+            return conn;
+        } catch (SQLException ex) {
+            System.out.println("Ошибка соединения " + ex.getMessage());
+            return null;
+        }
     }
+
+    private static boolean loadDriver() {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver");
+            System.out.println("Драйвер загружен");
+            return true;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("не найден драйвер "
+                    + ex.getMessage());
+            return false;
+        }
+    }
+
+    private static HashMap<String, String> getQueries(String path) {
+        LinkedList<String> lines = new LinkedList<>();
+        try (InputStream is = ClassLoader.getSystemResourceAsStream(path);
+                BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String s;
+            while ((s = br.readLine()) != null) {
+                lines.add(s);
+                 System.out.println("строка:" + s);
+            }
+        } catch (IOException ex) {
+            System.out.println("Ошибка чтения " + path);
+        }
+        HashMap<String, String> hss = new HashMap<>();
+        boolean entryStarted = false;
+        String key = null;
+        StringBuilder sb = new StringBuilder();
+        for (String line : lines) {
+            if (line.startsWith("-->")) {
+                entryStarted = true;
+                key = line.substring(3).trim();
+            } else {
+                if (entryStarted) {
+                    sb.append(line);
+                }
+                if (line.trim().endsWith(";")) {
+                    entryStarted = false;
+                    hss.put(key, sb.toString());
+                    sb.delete(0, sb.length());
+                }
+            }
+        }
+        return hss;
     
+    }
+
+    private static void printAllQuieries(HashMap<String, String> sqlMap) {
+        for (Map.Entry<String, String> entry : sqlMap.entrySet()) {
+            System.out.println("имя: " + entry.getKey()
+                    + "\nзапрос: " + entry.getValue());
+        }
+    }
 }
